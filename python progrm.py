51050b799766044831b58ddc6b46407d8ff5ec5a
suryaprakash-Gadi <suryagadi385@gmail.com>
2025-01-04 22:41:31 +0530

    added comments to it

--- python progrm.py
+++ python progrm.py
@@ -1,7 +1,12 @@
-a=int(input("enter a number")) #input from user
-for i in range(0,a+1):
+#code hrer
+
+#hello coder
+given_input=int(input("enter a number")) #input from user
+for i in range(0,given_input+1):
   if i %2==0:
     print(i,end=" ") #output of the  code
   else:
     
     print(i) #output of the else
+    
+#happy learning////
